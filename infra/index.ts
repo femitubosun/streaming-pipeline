57aa4783ad916a372c9f1e4aabe28c6288727c8f
Femi Olatubosun | ƒa†3 <thefemiolatubosun@gmail.com>
2026-05-18 01:44:17 +0100

    feat(infra): add redpanda to infra

--- infra/index.ts
+++ infra/index.ts
@@ -41,6 +41,47 @@ const apiImage = new docker.Image("api", {
 //   ],
 //   mustRun: true,
 // });
+//
+
+const redpanda = new docker.Container("redpanda", {
+  image: "docker.redpanda.com/redpandadata/redpanda:v26.1.8",
+  name: "redpanda",
+  networksAdvanced: [{ name: network.name }],
+  ports: [
+    {
+      internal: 9092,
+      external: 9092,
+    },
+    // {
+    //   internal: 9644,
+    //   external: 9644,
+    // },
+  ],
+  command: [
+    "redpanda",
+    "start",
+    "--mode",
+    "dev-container",
+    "--smp",
+    "1",
+    "--memory",
+    "1G",
+    "--overprovisioned",
+    "--kafka-addr",
+    "internal://0.0.0.0:9092",
+    "--advertise-kafka-addr",
+    "internal://redpanda:9092",
+  ],
+  memory: 2048,
+  mustRun: true,
+  healthcheck: {
+    tests: ["CMD", "curl", "-f", "http://localhost:9644/v1/status/ready"],
+    interval: "5s",
+    timeout: "3s",
+    retries: 5,
+    startPeriod: "10s",
+  },
+});
 
 const api = new docker.Container("api", {
   image: apiImage.imageName,
